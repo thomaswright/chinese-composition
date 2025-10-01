a1431bf746e1592c4dfa18b25ae3e78ab2b5dafc
Thomas Wright <thomaswright1@gmail.com>
2025-09-30 22:42:26 -0600

    add react compiler

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        // Enable the React Compiler via the Babel plugin
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
